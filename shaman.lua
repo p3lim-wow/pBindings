2c5ad6b74e27b03daba50ae3878c7fb2eac368ac
Adrian L Lange <adrianlund@gmail.com>
2010-06-10 20:25:22 +0200

    Merge the shared table into the main one

--- shaman.lua
+++ shaman.lua
@@ -1,18 +1,8 @@
 if(select(2, UnitClass('player')) ~= 'SHAMAN') then return end
-local _, bindings = ...
 
-local shared = {
-	ctrl = {
-		's|Call of the Elements',
-		's|Call of the Ancestors',
-		's|Call of the Spirits',
-		's|Totemic Recall'
 
-		BUTTON3 = 'm|/dimount\n/leavevehicle\n/cast Celestial Steed'
-	}
-}
 
-oBindings:RegisterKeyBindings('Restoration', shared, {
+oBindings:RegisterKeyBindings('Restoration', {
 	's|Riptide',
 	's|Chain Heal',
 	's|Lesser Healing Wave',
@@ -23,6 +13,15 @@ oBindings:RegisterKeyBindings('Restoration', shared, {
 		's|Earth Shield',
 		's|Mana Tide Totem',
 		's|Ancestral Spirit'		
+	},
+
+	ctrl = {
+		's|Call of the Elements',
+		's|Call of the Ancestors',
+		's|Call of the Spirits',
+		's|Totemic Recall',
+
+		BUTTON3 = 'm|/dimount\n/leavevehicle\n/cast Celestial Steed'
 	},
 
 	NUMPAD1 = 's|Mana Tide Totem',
