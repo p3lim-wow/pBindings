c3c2181718689a1139364e352736acdd15e60ad7
p3lim <adrianlund@gmail.com>
2011-01-03 22:42:05 +0100

    Add a base layout

---
+++ base.lua
@@ -0,0 +1,26 @@
+local _, bindings = ...
+
+bindings.base = {
+	W = 'MOVEFORWARD',
+	A = 'STRAFELEFT',
+	S = 'MOVEBACKWARD',
+	D = 'STRAFERIGHT',
+	
+	B = 'OPENALLBAGS',
+	G = 'TOGGLEGUILDTAB',
+	O = 'TOGGLEFRIENDSTAB',
+
+	ctrl = {
+		R = 'TOGGLERAIDTAB',
+	},
+
+	NUMPADMINUS = 'TOGGLEAUTORUN',
+
+	UP = 'RAIDTARGET8',
+	DOWN = 'RAIDTARGET7',
+	LEFT = 'RAIDTARGET1',
+	RIGHT = 'RAIDTARGET2',
+
+	F12 = 'm|/run ReloadUI()',
+}
+	
---
+++ druid.lua
@@ -0,0 +1,64 @@
+local _, bindings = ...
+
+pBindings:RegisterKeyBindings('Feral Combat', bindings.base, {
+	's|Lifebloom',
+	'm|/cast [worn:Fishing Pole,nochanneling] Fishing; Regrowth',
+	's|Healing Tough',
+	's|Tranquility',
+
+	F = 's|Rebirth',
+
+	BUTTON3 = 's|Darkflight',
+	BUTTON4 = 's|/focus\n/cast Entangling Roots',
+	BUTTON5 = 's|/focus\n/cast Hibernate',
+
+	cat = {
+		'm|/cast [stealth] Pounce; Rake',
+		'm|/cast [stealth] Ravage; Shred',
+		's|Swipe(Cat Form)',
+		's|Mangle(Cat Form)',
+
+		F = 's|Savage Roar',
+
+		BUTTON3 = 's|Prowl',
+		BUTTON4 = 's|Rip',
+		BUTTON5 = 's|Ferocious Bite',
+	},
+
+	bear = {
+		's|Lacerate',
+		's|Maul',
+		'm|/castsequence reset=combat Trash(Bear Form), Swipe(Bear Form)',
+		's|Mangle(Bear Form)',
+
+		F = 's|Challenging Roar',
+
+		BUTTON3 = 's|Enrage',
+		BUTTON4 = 's|Growl',
+		BUTTON5 = 's|Bash',
+	},
+
+	shift = {
+		'm|/cast [form:1] Frenzied Regrowth; [form:3] Tiger\'s Fury; Revive',
+		'm|/cast [form:2] Demoralizing Roar; [form:3] Dash; Mark of the Wild',
+		'm|/cast [form:1/3] Berserk; Innervate',
+
+		F = 'm|/cast [form:1] Feral Charge(Bear Form); [form:3] Feral Charge(Cat Form)',
+
+		BUTTON3 = 'm|/cast Barkskin\n/cast [form:1/3] Survival Instincts(Cat or Bear Form)',
+		BUTTON4 = 'm|/cast [form:1/3] Faerie Fire (Feral)(Feral); Faerie Fire',
+		BUTTON5 = 'm|/cast [form:1] Skull Bash(Bear Form); [form:3] Skull Bash(Cat Form); Cyclone',
+	},
+
+	ctrl = {
+		's|Bear Form',
+		's|Cat Form',
+		'm|/cast [swimming] Aquatic Form; [flyable] Swift Flight Form',
+		's|Travel Form',
+
+		F = 'm|/dismount\n/cast Survey',
+		
+		BUTTON3 = 'm|/dismount\n/leavevehicle\/cast [flyable] Time-Lost Proto-Drake; Running Wild',
+		BUTTON4 = 's|Thorns',
+	},
+})
