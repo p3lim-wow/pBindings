832f68eaf7be430b82a07322ffc19c53db788f25
Adrian L Lange <adrianlund@gmail.com>
2011-10-03 22:43:56 +0200

    I sometimes play my hunter too

---
+++ hunter.lua
@@ -0,0 +1,37 @@
+local _, bindings = ...
+
+pBindings:RegisterKeyBindings('Marksmanship', bindings.base, {
+	's|Chimera Shot',
+	's|Steady Shot',
+	's|Aimed Shot',
+	's|Serpent Sting',
+
+	Q = 'm|/cast Rapid Fire\n/cast Readiness\n/cast Lifebloom',
+	E = 's|Arcane Shot',
+
+	BUTTON3 = 's|Hunter\'s Mark',
+	BUTTON4 = 's|Disengage',
+	BUTTON5 = 's|Silencing Shot',
+
+	shift = {
+		'm|/focus [help]\n/stopmacro [help]\n/cast [@focus,help][@pet,exists] Misdirection',
+		's|Trap Launcher',
+		's|Feign Death',
+		's|Shadowmeld',
+
+		Q = 's|Multi-Shot',
+		E = 's|Kill Shot',
+	},
+
+	alt = {
+		's|Immolation Trap',
+		's|Snake Trap',
+		's|Freezing Trap',
+		's|Explosive Trap',
+		's|Ice Trap',
+	},
+
+	ctrl = {
+		BUTTON3 = 'm|/dismount\n/leavevehicle\n/cast Snowy Gryphon',
+	},
+})
