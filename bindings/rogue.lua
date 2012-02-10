cc1b251cb2154b2013e3e8438c1a87efb27aaca0
Adrian L Lange <adrianlund@gmail.com>
2012-02-10 17:34:48 +0100

    Add assassination keybindings

--- bindings/rogue.lua
+++ bindings/rogue.lua
@@ -2,27 +2,20 @@ if(select(2, UnitClass('player')) ~= 'ROGUE') then return end
 
 local _, bindings = ...
 
-pBindings:RegisterKeyBindings('Combat', bindings.base, {
-	'm|/startattack\n/cast Revealing Strike',
-	'm|/startattack\n/cast Sinister Strike',
-	'm|/startattack\n/cast Fan of Knives',
-	's|Redirect',
-	's|Feint',
+local shared = {
+	[3] = 'm|/startattack\n/cast Fan of Knives',
+	[4] = 's|Feint',
 
-	Q = 's|Adrenaline Rush',
-	E = 's|Killing Spree',
 	T = 's|Recuperate',
 	F = 's|Slice and Dice',
 
 	BUTTON3 = 's|Stealth',
-	BUTTON4 = 's|Eviscerate',
 	BUTTON5 = 's|Rupture',
 
 	stealth = {
 		's|Distract',
-		's|Cheap Shot',
-		's|Sap',
-		's|Pick Pocket',
+		[3] = 's|Sap',
+		[4] = 's|Pick Pocket',
 	},
 
 	shift = {
@@ -33,7 +26,6 @@ pBindings:RegisterKeyBindings('Combat', bindings.base, {
 		's|Blind',
 
 		Q = 'm|/cast Combat Readiness\n/cast Evasion',
-		E = 's|Blade Flurry',
 		T = 's|Throw',
 		F = 'm|/focus [help]\n/stopmacro [help]\n/cast [@focus,help] Tricks of the Trade',
 
@@ -43,5 +35,39 @@ pBindings:RegisterKeyBindings('Combat', bindings.base, {
 
 	ctrl = {
 		BUTTON3 = 'm|/dismount\n/leavevehicle\n/cast [flyable] Tyrael\'s Charger; Swift Mistsaber',
+	},
+}
+
+pBindings:RegisterKeyBindings('Combat', bindings.base, shared, {
+	'm|/startattack\n/cast Revealing Strike',
+	'm|/startattack\n/cast Sinister Strike',
+
+	[5] = 's|Redirect',
+
+	Q = 's|Adrenaline Rush',
+	E = 's|Killing Spree',
+
+	BUTTON4 = 's|Eviscerate',
+
+	stealth = {
+		[2] = 's|Cheap Shot',
+	},
+
+	shift = {
+		E = 's|Blade Flurry',
+	},
+})
+
+pBindings:RegisterKeyBindings('Assassination', bindings.base, shared, {
+	'm|/startattack\n/cast Backstab',
+	'm|/startattack\n/cast Mutilate',
+
+	Q = 's|Vendetta',
+	E = 's|Cold Blood',
+
+	BUTTON4 = 's|Envenom',
+
+	stealth = {
+		[2] = 's|Garrote',
 	},
 })
