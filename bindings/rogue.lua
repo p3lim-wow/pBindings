505190ee84dc07fe06bd8db8ccbee6d50ead0b7b
Adrian L Lange <adrianlund@gmail.com>
2012-03-15 04:10:04 +0100

    Rogue updates

--- bindings/rogue.lua
+++ bindings/rogue.lua
@@ -2,72 +2,47 @@ if(select(2, UnitClass('player')) ~= 'ROGUE') then return end
 
 local _, bindings = ...
 
-local shared = {
-	[3] = 'm|/startattack\n/cast Fan of Knives',
-	[4] = 's|Feint',
+pBindings:RegisterKeyBindings('Combat', bindings.base, {
+	'm|/startattack\n/cast Revealing Strike',
+	'm|/startattack\n/cast Sinister Strike',
+	'm|/startattack\n/cast Fan of Knives',
+	's|Feint',
+	's|Redirect',
 
+	Q = 'm|/cast Adrenaline Rush\n/use 13',
+	E = 's|Killing Spree',
 	T = 's|Recuperate',
 	F = 's|Slice and Dice',
 
 	BUTTON3 = 's|Stealth',
-	BUTTON5 = 's|Rupture',
+	BUTTON4 = 's|Eviscerate',
+	BUTTON5 = 's|Kidney Shot',
 
 	stealth = {
 		's|Distract',
-		[3] = 's|Sap',
-		[4] = 's|Pick Pocket',
+		's|Cheap Shot',
+		's|Sap',
+		'm|/cleartarget\n/targetenemy\n/cast Pick Pocket',
 	},
 
 	shift = {
-		's|Vanish',
+		's|Smoke Bomb',
 		's|Sprint',
 		's|Cloak of Shadows',
 		's|Shadowmeld',
 		's|Blind',
 
 		Q = 'm|/cast Combat Readiness\n/cast Evasion',
+		E = 's|Blade Flurry',
 		T = 's|Throw',
 		F = 'm|/focus [help]\n/stopmacro [help]\n/cast [@focus,help] Tricks of the Trade',
 
+		BUTTON3 = 's|Vanish',
 		BUTTON4 = 's|Kick',
-		BUTTON5 = 's|Kidney Shot',
+		BUTTON5 = 's|Dismantle',
 	},
 
 	ctrl = {
-		BUTTON3 = 'm|/dismount\n/leavevehicle\n/cast [flyable] Tyrael\'s Charger; Swift Mistsaber',
-	},
-}
-
-pBindings:RegisterKeyBindings('Combat', bindings.base, shared, {
-	'm|/startattack\n/cast Revealing Strike',
-	'm|/startattack\n/cast Sinister Strike',
-
-	[5] = 's|Redirect',
-
-	Q = 's|Adrenaline Rush',
-	E = 's|Killing Spree',
-
-	BUTTON4 = 's|Eviscerate',
-
-	stealth = {
-		[2] = 's|Cheap Shot',
-	},
-
-	shift = {
-		E = 's|Blade Flurry',
-	},
-})
-
-pBindings:RegisterKeyBindings('Assassination', bindings.base, shared, {
-	'm|/startattack\n/cast Backstab',
-	'm|/startattack\n/cast Mutilate',
-
-	Q = 's|Vendetta',
-	E = 's|Cold Blood',
-
-	BUTTON4 = 's|Envenom',
-
-	stealth = {
-		[2] = 's|Garrote',
+		BUTTON3 = 'm|/dismount\n/leavevehicle\n/cast [flyable] Golden Gryphon; Swift Mistsaber',
 	},
 })
