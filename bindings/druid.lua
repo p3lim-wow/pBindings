fa4bfaf3ce32de3b0f3d08eb055462d52433d075
Adrian L Lange <adrianlund@gmail.com>
2012-07-28 22:21:36 +0200

    Quick and dirty update to the druid keybindings

--- bindings/druid.lua
+++ bindings/druid.lua
@@ -2,80 +2,65 @@ if(select(2, UnitClass('player')) ~= 'DRUID') then return end
 
 local _, bindings = ...
 
-pBindings:RegisterKeyBindings('Feral Combat', bindings.base, {
-	's|Lifebloom',
-	'm|/cast [worn:Fishing Pole] Fishing; [nochanneling] Regrowth',
+pBindings:RegisterKeyBindings('Feral', bindings.base, {
 	's|Rejuvenation',
-	's|Nourish',
-	's|Entangling Roots',
+	's|Healing Touch',
+	's|Mark of the Wild',
+	's|Symbiosis',
+	's|Revive',
 
-	BUTTON3 = 'm|',
-	BUTTON4 = 'm|/focus\n/cast Entangling Roots',
-	BUTTON5 = 'm|/focus\n/cast Hibernate',
+	BUTTON3 = 's|Prowl',
 
 	cat = {
 		's|Rake',
 		's|Shred',
-		's|Swipe(Cat Form)',
-		's|Mangle(Cat Form)',
-		's|Maim(Cat Form)',
+		's|Swipe',
+		's|Mangle',
+		's|Trash',
 
-		Q = 'm|/cast Tiger\'s Fury\n/cast Berserk\n/use 13',
-		E = 'm|/cast Tiger\'s Fury\n/cast [form:3] Ravage!',
+		Q = 'm|/cast Tiger\'s Fury\n/cast Berserk',
+		E = 's|Tiger\'s Fury',
 		F = 's|Savage Roar',
-		T = 'm|/cast Feral Charge(Cat Form)\n/cast [form:3] Ravage!',
 
 		BUTTON3 = 's|Prowl',
 		BUTTON4 = 's|Rip',
 		BUTTON5 = 's|Ferocious Bite',
 	},
 
-	stealth = {
-		[2] = 'm|/cleartarget\n/targetenemyplayer\n/cast Pounce',
-	},
-
 	bear = {
 		's|Lacerate',
 		's|Maul',
-		's|Mangle(Bear Form)',
-		's|Thrash(Bear Form)',
-		's|Swipe(Bear Form)',
+		's|Thrash',
+		's|Mangle',
+		's|Swipe',
 
 		Q = 's|Berserk',
-		E = 's|Barkskin',
-		F = 's|Challenging Roar',
-		T = 's|Feral Charge(Bear Form)',
+		E = 's|Might of Ursoc',
+		F = 's|Frenzied Regeneration',
 
-		BUTTON3 = 's|Enrage',
 		BUTTON4 = 's|Growl',
-		BUTTON5 = 's|Bash',
 	},
 
 	shift = {
-		'm|/cast Innervate',
-		'm|/cast [form:1] Demoralizing Roar; [form:3] Dash; Mark of the Wild',
+		's|Innervate',
+		's|Dash',
 		's|Tranquility',
-		's|Shadowmeld',
-		'm|/cast [form:3] Stampeding Roar(Cat Form); [form:1] Stampeding Roar(Bear Form)',
+		's|Stampeding Roar',
+		's|Mighty Bash',
 
-		Q = 'm|/cast [noform:1] Barkskin\n/cast [form:1/3] Survival Instincts(Cat or Bear Form)',
-		E = 'm|/cast [form:1] Frenzied Regeneration',
-		F = 'm|/cast [spec:1] Rebirth; Cyclone',
-		T = 'i|14',
-		X = 's|Nature\'s Grasp',
+		Q = 's|Barkskin',
+		E = 's|Renewal',
+		F = 's|Rebirth',
 
-		BUTTON4 = 'm|/cast [form:1/3] Faerie Fire (Feral)(Feral); Faerie Fire',
-		BUTTON5 = 'm|/cast [form:1] Skull Bash(Bear Form); [form:3] Skull Bash(Cat Form); Cyclone',
+		BUTTON3 = 's|Shadowmeld',
+		BUTTON4 = 's|Faerie Fire',
+		BUTTON5 = 's|Skull Bash',
 	},
 
 	ctrl = {
 		's|Bear Form',
 		's|Cat Form',
-		'm|/cast [swimming] Aquatic Form; [flyable] Swift Flight Form',
+		'm|/cast [swimming] Aquatic Form; [flyable] Swift Flight Form; Travel Form',
 		's|Travel Form',
-
-		F = 's|Survey',
-		
-		BUTTON3 = 'm|/dismount\n/leavevehicle\n/cast [mod:alt] Traveler\'s Tundra Mammoth; [flyable] Twilight Harbinger; Mekgineer\'s Chopper',
 	},
 })
